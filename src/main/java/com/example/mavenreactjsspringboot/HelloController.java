e25332ba581ec93f6dc32e6e97dd3e1edcfaefda
Prakash Singh <33134755+singhprakash9229@users.noreply.github.com>
2021-10-04 06:25:47 +0530

    Add files via upload
    first controller

---
+++ src/main/java/com/example/mavenreactjsspringboot/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.mavenreactjsspringboot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController
+{
+ @GetMapping("/hello")
+  public String greetings() {
+    return "Hello-world";
+  }
+}
